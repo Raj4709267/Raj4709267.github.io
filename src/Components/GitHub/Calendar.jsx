681b2c7683c6101ce2fcd8ae011e541f9b606ca4
Raj kumar mahto <rk2519962@gmail.com>
2022-10-25 09:03:07 +0530

    update

--- src/Components/GitHub/Calendar.jsx
+++ src/Components/GitHub/Calendar.jsx
@@ -7,7 +7,7 @@ function Calendar() {
     <div
       style={{
         padding: "20px",
-        margin: "30px 0 50px 0",
+        margin: "30px 0 0 0",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
@@ -22,11 +22,36 @@ function Calendar() {
       >
         Git<span style={{ color: "#ffa31a" }}>Hub</span> Calendar
       </p>
-      <div>
+      <div className={style.calender}>
         <GitHubCalendar username="Raj4709267" />
+      </div>
+      <div className={style.stats}>
+        <a href="https://github.com/Raj4709267">
+          <img
+            align="center"
+            src="https://github-readme-streak-stats.herokuapp.com?user=Raj4709267&theme=dark"
+            width="100%"
+            alt="stats"
+          />
+        </a>
+
+        <a href="https://github.com/Raj4709267">
+          <img
+            align="center"
+            src="https://github-readme-stats.vercel.app/api?username=Raj4709267&theme=dark&count_private=true&show_icons=true
+          "
+          alt="stats"
+          />
+        </a>
+        <a href="https://github.com/Raj4709267">
+          <img
+            align="center"
+            src="https://github-readme-stats.vercel.app/api/top-langs/?username=Raj4709267&theme=dark"
+            alt="stats"
+          />
+        </a>
       </div>
     </div>
   );
 }
-
 export default Calendar;
